7dc2c033a9e4bf59d905d183e2aad33c373747e4
Samuel Brito <sambriton7@gmail.com>
2023-10-09 19:27:13 -0300

    Adding the shadcn/ui and the next-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from './components/providers/ThemeProvider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>      
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider></body>
     </html>
     </ClerkProvider>
     
