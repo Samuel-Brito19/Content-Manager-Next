49a4566e1fc5bfcd17257ede8fa24a0fd6e7ff5f
Samuel Brito <sambriton7@gmail.com>
2023-10-09 20:01:11 -0300

    Creating a Logo component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,20 @@
+import { UserButton } from '@clerk/nextjs'
 import React, { ReactNode } from 'react'
+import Logo from '../components/Logo'
+import ThemeSwitcher from '../components/ThemeSwitcher'
 
 function Layout({children}:{children:ReactNode}) {
   return (
-    <div className='flex flex-col min-h-screen min-w-full bg-backgroud max-h-screen'>{children}</div>
+    <div className='flex flex-col min-h-screen min-w-full bg-backgroud max-h-screen'>{children}
+    <nav>Nav
+        <Logo/>
+        <ThemeSwitcher/>
+        <UserButton afterSignOutUrl='/sign-in' />
+    </nav>
+    <main className='flex w-full flex-grow'>
+        {children}
+    </main>
+    </div>
   )
 }
 
---
+++ app/components/ThemeSwitcher.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { SunIcon } from "@radix-ui/react-icons"
+import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs"
+import { useTheme } from "next-themes"
+import React, { useEffect, useState } from "react"
+
+function ThemeSwitcher() {
+    const {theme, setTheme} = useTheme()
+    const [mounted, setMouted] =useState(false)
+
+    useEffect(() => {
+        setMouted(true)
+    },[])
+
+    if(!mounted) return null
+    return (
+        <Tabs defaultValue={theme}>
+            <TabsList className="border">
+            <TabsTrigger value="light"
+            onClick={()=> setTheme("light")}>
+                <SunIcon className="h-[1.2rem] w-[1.2rem]"/>
+            </TabsTrigger>
+            </TabsList>
+            
+        </Tabs>
+    )
+}
+
+export default ThemeSwitcher
